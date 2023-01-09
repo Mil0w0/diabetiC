f0808a4274719d9992af7e0da68d9fef21dac2d9
Milo <lhilderal@myges.fr>
2023-01-09 14:13:41 +0100

    can get glycemia input from user and updating docs

--- glycemia/glycemia.c
+++ glycemia/glycemia.c
@@ -5,3 +5,28 @@
 #include <string.h>
 #include "glycemia.h"
 
+//text variables to be put in an env file later
+char defaultUnit[] = "Default glycemia unit is g/L.\n";
+char askForGlycemia[] = "Enter current glycemia : ";
+
+//get glycemia from user
+double inputsGlycemia(){
+    double glycemia;
+    double irrealMax = 100; //but can be different if unit is different.
+    do {
+        printf(defaultUnit);
+        printf(askForGlycemia);
+        scanf("%lf", &glycemia);
+        printf("\n");
+    } while (glycemia < 0 || glycemia > irrealMax);
+    //need to write double with a dot not comma. how do we check that ?
+
+    return glycemia;
+}
+
+
+//test code
+int main(int argc, char **argv){
+    double glycemia = inputsGlycemia();
+    printf("%.2lf", glycemia);
+}
