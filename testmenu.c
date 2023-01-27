930a0b4fa9cb4ce8db5dcc0d66248fd2e6d7bc24
Nino <ninoplane@gmail.com>
2023-01-27 16:50:54 +0100

    User creation, connection and menu done

--- testmenu.c
+++ testmenu.c
@@ -5,16 +5,24 @@
 #include "sqlite3.h"
 #include <time.h>
 
-static int callback(void *NotUsed, int argc, char **argv, char **azColName) {
+static int callback(void *NotUsed, int argc, char **argv, char **azColName)
+{
    int i;
-   for(i = 0; i<argc; i++) {
+   for(i = 0; i<argc; i++)
+   {
       printf("%s = %s\n", azColName[i], argv[i] ? argv[i] : "NULL");
    }
    printf("\n");
    return 0;
 }
 
-bool LogIn(char *username, char *password, sqlite3 *db) {
+static int checkUser(void *NotUsed, int argc, char **argv, char **azColName)
+{
+    return 1;
+}
+
+bool LogIn(char *username, char *password, sqlite3 *db)
+{
     char *zErrMsg = 0;
     int rc;
     char *sql;
@@ -23,19 +31,24 @@ bool LogIn(char *username, char *password, sqlite3 *db) {
 
     rc = sqlite3_exec(db, sql, callback, 0, &zErrMsg);
 
-    if( rc != SQLITE_OK ) {
+    if( rc != SQLITE_OK )
+    {
         fprintf(stderr, "SQL error: %s\n", zErrMsg);
         sqlite3_free(zErrMsg);
-    } else {
-        if(rc == 0) {
+    } else 
+    {
+        if(rc == 0) 
+        {
             return false;
-        }else if (rc == 1) {
+        }else if (rc == 1) 
+        {
             return true;
         }
     }
 }
 
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
 
     sqlite3 *db;
     char *zErrMsg = 0;
@@ -50,11 +63,13 @@ int main(int argc, char **argv) {
     //CREATE  TEST DATABASE;
     rc = sqlite3_open("test.db", &db);
 
-    if( rc ) {
+    if( rc )
+    {
         fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
         return(0);
-    } else {
-        fprintf(stderr, "Opened database successfully\n");
+    } else
+    {
+        printf("\n");
     }
 
     //CREATE A USER TABLE IF NOT EXISTS;
@@ -69,47 +84,83 @@ int main(int argc, char **argv) {
     /* Execute SQL statement */
     rc = sqlite3_exec(db, sql, callback, 0, &zErrMsg);
 
-    if( rc != SQLITE_OK ) {
+    if( rc != SQLITE_OK )
+    {
         fprintf(stderr, "SQL error: %s\n", zErrMsg);
         sqlite3_free(zErrMsg);
     }
 
-    // Print the table
-    sql = "SELECT * FROM USERS";
+    // Print the table users for debug only
+    // sql = "SELECT * FROM USERS";
 
-    rc = sqlite3_exec(db, sql, callback, 0, &zErrMsg);
+    // rc = sqlite3_exec(db, sql, callback, 0, &zErrMsg);
 
-    if( rc != SQLITE_OK ) {
-        fprintf(stderr, "SQL error: %s\n", zErrMsg);
-        sqlite3_free(zErrMsg);
-    }
+    // if( rc != SQLITE_OK )
+    // {
+    //     fprintf(stderr, "SQL error: %s\n", zErrMsg);
+    //     sqlite3_free(zErrMsg);
+    // }
 
-    do {
+    do 
+    {
         // Case 1: User is not connected
-        if(connected == 0) {
+        if(connected == 0)
+        {
             printf("Welcome to your glycemia database, please choose an option:\n");
             printf("1. Log in with your user\n");
             printf("2. Create a new user\n");
-            printf("3. Exit\n");
+            printf("9. Exit\n");
             scanf("%d", &choice);
 
-            if(choice == 1) {
-                printf("Please enter your username:\n");
+            if(choice == 1)
+            {
+                printf("\nPlease enter your username:\n");
                 scanf("%s", &username);
+                strcat(username, "\0");
+                printf("\n");
+
                 printf("Please enter your password:\n");
-                scanf("%s", &password);
-                isUserValid = LogIn(username, password, db); 
-                if(isUserValid) {
-                    printf("You are now connected!\n");
-                    connected = 1;
-                }else {
-                    printf("Wrong username or password, please try again!\n");
+                fflush(stdin);
+                char* input = malloc(30);
+                char* getPassword = malloc(30);
+                getPassword = fgets(input, 30, stdin);
+                for(int i = 0; i < strlen(getPassword); i++)
+                {
+                    if(getPassword[i] == '\n')
+                    {
+                        getPassword[i] = getPassword [i + 1];
+                    }
                 }
-            }else if(choice == 2) {
-                // sql = "SELECT COUNT * from USERS"
+                printf("\n");
+
+                char sql[200] = "SELECT * FROM USERS WHERE username = '";
+                strcat(sql, username);
+                strcat(sql, "' AND password = '");
+                strcat(sql, getPassword);
+                strcat(sql, "'");
+
+                rc = sqlite3_exec(db, sql, checkUser, 0, &zErrMsg);
+
+                if( rc == SQLITE_OK )
+                {
+                    printf("Wrong username or password, please try again\n\n");
+                    sqlite3_free(zErrMsg);
+                } else
+                {
+                    printf("You are now connected !\n");
+                    printf("Welcome %s ^^\n\n", username);
+                    connected = 1;
+                }
+
+                free(input);
+                free(getPassword);
+
+
+            }else if(choice == 2)
+            {
 
                 char sqltest[200] = "INSERT INTO USERS (USERNAME, PASSWORD, AGE, CREATED_AT) VALUES ('\0";
-                printf("Please enter your username:\n");
+                printf("\nPlease enter your username:\n");
                 scanf("%s", &username);
                 printf("\n");
                 strcat(username, "\0");
@@ -119,7 +170,8 @@ int main(int argc, char **argv) {
                 char* input = malloc(30);
                 char* getPassword = malloc(30);
                 getPassword = fgets(input, 30, stdin);
-                for(int i = 0; i < strlen(getPassword); i++) {
+                for(int i = 0; i < strlen(getPassword); i++)
+                {
                     if(getPassword[i] == '\n') {
                         getPassword[i] = getPassword [i + 1];
                     }
@@ -143,28 +195,46 @@ int main(int argc, char **argv) {
 
                 rc = sqlite3_exec(db, sqltest, callback, 0, &zErrMsg);
 
-                if( rc != SQLITE_OK ) {
+                if( rc != SQLITE_OK )
+                {
                     fprintf(stderr, "SQL error: %s\n", zErrMsg);
                     sqlite3_free(zErrMsg);
-                } else {
+                } else
+                {
                     fprintf(stdout, "User created successfully\n");
+                    printf("You are now connected !\n");
+                    printf("Welcome %s ^^\n\n", username);
                     connected = 1;
                 }
 
-            }else if(choice == 3) {
+            }else if(choice == 9)
+            {
                 printf("Goodbye!\n");
                 exit(0);
             }
         // Case 2: User is connected
-        }else if(connected == 1) {
+        }else if(connected == 1)
+        {
             printf("Now, please choose an option:\n");
             printf("1. Add an other glycemia log\n");
             printf("2. See your glycemia logs\n");
             printf("3. See your glycemia logs for a specific date\n");
             printf("4. Log out\n");
+            printf("9. Exit\n");
             scanf("%d", &choice);
+
+            if(choice == 4)
+            {
+                printf("\nYou are now disconnected\n\n");
+                printf("Goodbye %s !\n\n", username);
+                connected = 0;
+            }else if(choice == 9)
+            {
+                printf("Goodbye!\n");
+                exit(0);
+            }
         }
-    }while(choice != 3);
+    }while(choice != 9);
 
     return 0;
 }
